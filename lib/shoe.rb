27dc29b05d7fa395622e3cc44457d9d059bda964
Ethel C <cofresi.ethel@gmail.com>
2018-07-03 19:16:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,8 +3,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   def cobble
