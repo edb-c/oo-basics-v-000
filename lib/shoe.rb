832c0a9a72cc244e0b3a15c36f1ab9034aa191f6
Ethel C <cofresi.ethel@gmail.com>
2018-07-03 19:05:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Book
-  attr_accessor :color, :size, :material, :
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
  
   def initialize(title)
