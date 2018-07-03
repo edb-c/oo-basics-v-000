7f78a7cbaf9f51b336dbac2b52ddff7bc489ddf9
Ethel C <cofresi.ethel@gmail.com>
2018-07-03 19:23:30 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,9 @@ class Shoe
   end
  
   def cobble
-    puts "Your shoe is as good as new!"
     shoe = Shoe.new("Nike")
     shoe.condition = "new"
+    puts "Your shoe is as good as new!"
   end
  
 end
